89d766384879f940cfe7a4dac7043f08e4c94481
Nestor Vera <nestorvera@me.com>
2019-01-12 18:54:00 +0100

    BREAKING CHANGE: Fix broken hooks since 3.2.12

--- includes/class-yearly-archive-facetwp-core.php
+++ includes/class-yearly-archive-facetwp-core.php
@@ -211,14 +211,14 @@ class Yearly_Archive_FacetWP_Core {
 		<script>
 		(function ($) {
 
-			wp.hooks.addAction( 'facetwp/load/yearly', function ( $this, obj ) {
+			FWP.hooks.addAction( 'facetwp/load/yearly', function ( $this, obj ) {
 				$this.find( '.facet-source' ).val( obj['source'] );
 				$this.find( '.type-yearly .facet-label-any' ).val( obj['label_any'] );
 				$this.find( '.type-yearly .facet-orderby' ).val( obj['orderby'] );
 				$this.find( '.type-yearly .facet-count' ).val( obj['count'] );
 			});
 
-			wp.hooks.addFilter( 'facetwp/save/yearly', function ( $this, obj ) {
+			FWP.hooks.addFilter( 'facetwp/save/yearly', function ( $this, obj ) {
 				obj['source'] = $this.find('.facet-source').val();
 				obj['label_any'] = $this.find('.type-yearly .facet-label-any').val();
 				obj['orderby'] = $this.find('.type-yearly .facet-orderby').val();
@@ -237,12 +237,12 @@ class Yearly_Archive_FacetWP_Core {
 	function front_scripts() { ?>
 		<script>
 			(function ($) {
-				wp.hooks.addAction('facetwp/refresh/yearly', function ($this, facet_name) {
+				FWP.hooks.addAction('facetwp/refresh/yearly', function ($this, facet_name) {
 					var val = $this.find('.facetwp-yearly').val();
 					FWP.facets[facet_name] = val ? [val] : [];
 				});
 
-				wp.hooks.addAction('facetwp/ready', function () {
+				FWP.hooks.addAction('facetwp/ready', function () {
 					$(document).on('change', '.facetwp-facet .facetwp-yearly', function () {
 						var $facet = $(this).closest('.facetwp-facet');
 						if ('' != $facet.find(':selected').val()) {
